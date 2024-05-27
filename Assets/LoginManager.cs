5f57f9e5eb71364dbdbc6a382f13bcc73db61e1b
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-05-27 14:39:14 -0300

    Login Manager

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Cuando el usuario presione el botón "Aceptar"
+//debe escribirse un mensaje en Consola
+//si el texto del InputPassword coincide
+//con una contraseña predefinida por ustedes
+//escribir "Access granted", sino escribir "Access denied"
+public class LoginManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
